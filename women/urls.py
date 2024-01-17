7854b1a190bc74d9ed59db6044248c32203a23a9
Paul Starodub <amorallex@gmail.com>
2024-01-17 20:58:03 +0200

    add women/urls.py, update view.py with different types of path

--- women/urls.py
+++ women/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from women import views
 
 urlpatterns = [
     path("", views.index, name="women"),
-    path("cats/", views.categories, name="categories"),
+    path("cats/<int:cat_id>/", views.categories, name="category"),
+    path(
+        "cats/<slug:cat_slug>/",
+        views.categories_by_slug,
+        name="category_by_slug",
+    ),
+    re_path(r"archive/(?P<year>[0-9]{4}/)", views.archive, name="archive"),
 ]
 
 app_name = "women"
--- women/views.py
+++ women/views.py
@@ -6,5 +6,13 @@ def index(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Women's page")
 
 
-def categories(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("<h2>categories</h2>")
+def categories(request: HttpRequest, cat_id: int) -> HttpResponse:
+    return HttpResponse(f"<h2>Category id:{cat_id}</h2>")
+
+
+def categories_by_slug(request: HttpRequest, cat_slug: int) -> HttpResponse:
+    return HttpResponse(f"<h2>Category slug:{cat_slug}</h2>")
+
+
+def archive(request: HttpRequest, year: int) -> HttpResponse:
+    return HttpResponse(f"<h2>Category year:{year}</h2>")

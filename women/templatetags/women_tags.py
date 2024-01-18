855e53061160d2c0eccc98e74aac48805ff44ba0
Paul Starodub <amorallex@gmail.com>
2024-01-18 23:50:03 +0200

    add list_categories.html + inclusion tag

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -9,3 +9,9 @@ register = template.Library()
 @register.simple_tag()
 def get_categories() -> List[Any]:
     return views.cats_db
+
+
+@register.inclusion_tag("women/list_categories.html")
+def show_categories() -> dict:
+    cats_db = views.cats_db
+    return {"cats": cats_db}

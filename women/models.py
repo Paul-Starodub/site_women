df24df47ddb1bc31b10a1d8877cffbb2e80946bd
Paul Starodub <amorallex@gmail.com>
2024-01-19 21:03:30 +0200

    update models.py with choice, add migrations

--- women/models.py
+++ women/models.py
@@ -5,16 +5,24 @@ from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self) -> QuerySet:
-        return super().get_queryset().filter(is_published=True)
+        return (
+            super().get_queryset().filter(is_published=Women.Status.PUBLISHED)
+        )
 
 
 class Women(models.Model):
+    class Status(models.IntegerChoices):
+        DRAFT = 0, "Draft"
+        PUBLISHED = 1, "Published"
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(
+        choices=Status.choices, default=Status.DRAFT
+    )
 
     objects = models.Manager()
     published = PublishedManager()

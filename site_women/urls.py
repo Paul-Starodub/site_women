17753771e41d9a155bd1664ee82b77a068870808
Paul Starodub <amorallex@gmail.com>
2024-01-20 18:15:05 +0200

    install django-debug-toolbar

--- site_women/urls.py
+++ site_women/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("women.urls", namespace="women")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
 handler404 = "women.views.page_not_found"

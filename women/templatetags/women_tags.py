50027a5bec5f9401ed6526352a3ab92adcad8acd
Paul Starodub <amorallex@gmail.com>
2024-01-20 17:47:57 +0200

    update women_tags.py with annotate

--- women/templatetags/women_tags.py
+++ women/templatetags/women_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models import Count
+
 from women.models import Category, TagPost
 
 register = template.Library()
@@ -6,10 +8,16 @@ register = template.Library()
 
 @register.inclusion_tag("women/list_categories.html")
 def show_categories(cat_selected: int = 0) -> dict:
-    cats_db = Category.objects.all()
+    cats_db = Category.objects.annotate(total=Count("posts")).filter(
+        total__gt=0
+    )
     return {"cats": cats_db, "cat_selected": cat_selected}
 
 
 @register.inclusion_tag("women/list_tags.html")
 def show_all_tags() -> dict:
-    return {"tags": TagPost.objects.all()}
+    return {
+        "tags": TagPost.objects.annotate(total=Count("tags")).filter(
+            total__gt=0
+        )
+    }

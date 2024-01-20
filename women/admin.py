e2cc691d19392d81964188148d77a814e08c527e
Paul Starodub <amorallex@gmail.com>
2024-01-20 21:22:57 +0200

    customize admin panel with custom method)

--- women/admin.py
+++ women/admin.py
@@ -1,4 +1,6 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import QuerySet
+from django.http import HttpRequest
 
 from women.models import Women, Category
 
@@ -16,10 +18,25 @@ class WomenAdmin(admin.ModelAdmin):
     ordering = ("-time_create", "title")
     list_editable = ("is_published",)
     list_per_page = 5
+    actions = ["set_published", "set_unpublished"]
 
     @admin.display(description="Short description", ordering="content")
     def brief_info(self, women: Women) -> str:
         return f"Description {len(women.content)} symbols"
+
+    @admin.action(description="Publish")
+    def set_published(self, request: HttpRequest, queryset: QuerySet) -> None:
+        count = queryset.update(is_published=Women.Status.PUBLISHED)
+        self.message_user(request, f"Changed {count} articles")
+
+    @admin.action(description="Unpublish")
+    def set_unpublished(
+        self, request: HttpRequest, queryset: QuerySet
+    ) -> None:
+        count = queryset.update(is_published=Women.Status.DRAFT)
+        self.message_user(
+            request, f"Unpublished {count} articles", messages.WARNING
+        )
 
 
 @admin.register(Category)

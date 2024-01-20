da63c02cd6c192aba4e18ea34e402860c8ac7241
Paul Starodub <amorallex@gmail.com>
2024-01-20 20:03:13 +0200

    update admin.py(old case)

--- women/admin.py
+++ women/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from women.models import Women
 
-admin.site.register(Women)
+
+class WomenAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "time_create", "is_published")
+
+
+admin.site.register(Women, WomenAdmin)

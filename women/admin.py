340cb12bfb5dd6143963517366d2cb60aa25b44e
Paul Starodub <amorallex@gmail.com>
2024-01-20 21:05:39 +0200

    customize admin panel with custom method

--- women/admin.py
+++ women/admin.py
@@ -5,11 +5,21 @@ from women.models import Women, Category
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "time_create", "is_published", "cat")
-    list_display_links = ("id", "title")
+    list_display = (
+        "title",
+        "time_create",
+        "is_published",
+        "cat",
+        "brief_info",
+    )
+    list_display_links = ("title",)
     ordering = ("-time_create", "title")
     list_editable = ("is_published",)
     list_per_page = 5
+
+    @admin.display(description="Short description", ordering="content")
+    def brief_info(self, women: Women) -> str:
+        return f"Description {len(women.content)} symbols"
 
 
 @admin.register(Category)

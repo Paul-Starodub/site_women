a1268405deb0c888a7ffa7d73771e8af991ab6ca
Paul Starodub <amorallex@gmail.com>
2024-01-21 15:58:33 +0200

    update admin.py with custom filter

--- women/admin.py
+++ women/admin.py
@@ -1,8 +1,26 @@
+from typing import Any, Tuple, List
+
 from django.contrib import admin, messages
 from django.db.models import QuerySet
 from django.http import HttpRequest
 
 from women.models import Women, Category
+
+
+class MarriedFilter(admin.SimpleListFilter):
+    title = "Woman status"
+    parameter_name = "status"
+
+    def lookups(
+        self, request: HttpRequest, model_admin: Any
+    ) -> List[Tuple[str, str]]:
+        return [("married", "Married"), ("single", "Single")]
+
+    def queryset(self, request: HttpRequest, queryset: QuerySet) -> QuerySet:
+        if self.value() == "married":
+            return queryset.filter(husband__isnull=False)
+        if self.value() == "single":
+            return queryset.filter(husband__isnull=True)
 
 
 @admin.register(Women)
@@ -19,6 +37,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ("is_published",)
     list_per_page = 5
     actions = ["set_published", "set_unpublished"]
+    search_fields = ("title__startswith", "cat__name")
+    list_filter = ("cat__name", "is_published", MarriedFilter)
 
     @admin.display(description="Short description", ordering="content")
     def brief_info(self, women: Women) -> str:

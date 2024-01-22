a650d219b36b099d9a2a6e13364187582604f6a2
Paul Starodub <amorallex@gmail.com>
2024-01-22 13:13:09 +0200

    update forms.py with label

--- women/forms.py
+++ women/forms.py
@@ -4,10 +4,12 @@ from women.models import Category, Husband
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
+    slug = forms.SlugField(max_length=255, label="URL")
     content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    is_published = forms.BooleanField(required=False, label="Status")
+    cat = forms.ModelChoiceField(
+        queryset=Category.objects.all(), label="Categories"
+    )
     husband = forms.ModelChoiceField(
         queryset=Husband.objects.all(), required=False
     )

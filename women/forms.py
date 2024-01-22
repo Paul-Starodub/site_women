fd0dd6f87ccee04561d1b3f2823d3c2cf66aae82
Paul Starodub <amorallex@gmail.com>
2024-01-22 15:33:07 +0200

    update addpage.html with form via loop for

--- women/forms.py
+++ women/forms.py
@@ -3,9 +3,13 @@ from women.models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
+    title = forms.CharField(
+        max_length=255, widget=forms.TextInput(attrs={"class": "form-input"})
+    )
     slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(), required=False)
+    content = forms.CharField(
+        widget=forms.Textarea(attrs={"cols": 50, "rows": 5}), required=False
+    )
     is_published = forms.BooleanField(
         required=False, label="Status", initial=True
     )

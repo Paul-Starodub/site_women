015bfeb2bf7f079e4a81c0c2020f7d42fce8a38f
Paul Starodub <amorallex@gmail.com>
2024-01-22 13:15:55 +0200

    update forms.py with initial

--- women/forms.py
+++ women/forms.py
@@ -6,7 +6,9 @@ class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
     slug = forms.SlugField(max_length=255, label="URL")
     content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False, label="Status")
+    is_published = forms.BooleanField(
+        required=False, label="Status", initial=True
+    )
     cat = forms.ModelChoiceField(
         queryset=Category.objects.all(),
         label="Categories",

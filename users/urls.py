f550d97350231be6f1fbdbbd1b69421555072fca
Paul Starodub <amorallex@gmail.com>
2024-01-26 21:14:30 +0200

    update views.py with LoginView

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from users import views
 
 
 urlpatterns = [
-    path("login/", views.login_user, name="login"),
+    path("login/", views.LoginUser.as_view(), name="login"),
     path("logout/", views.logout_user, name="logout"),
 ]
 
--- users/views.py
+++ users/views.py
@@ -1,27 +1,21 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse
-
-from users.forms import LoginUserForm
+from django.urls import reverse, reverse_lazy
 
 
-def login_user(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request,
-                username=cd["username"],
-                password=cd["password"],
-            )
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse("women:home"))
-    else:
-        form = LoginUserForm()
-    return render(request, "users/login.html", {"form": form})
+class LoginUser(LoginView):
+    form_class = AuthenticationForm
+    template_name = "users/login.html"
+    extra_context = {"title": "Login"}
+
+    def get_success_url(
+        self,
+    ) -> (
+        str
+    ):  # if we want to use our url instead default, or use LOGIN_REDIRECT_URL in settings.py
+        return reverse_lazy("women:home")
 
 
 def logout_user(request: HttpRequest) -> HttpResponse:

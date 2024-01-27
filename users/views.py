da618a672b47da852ece9d52e5b8cec131d6df69
Paul Starodub <amorallex@gmail.com>
2024-01-27 18:41:32 +0200

    update users/views.py & urls.py with ProfileUser

--- users/views.py
+++ users/views.py
@@ -1,8 +1,13 @@
+from typing import Optional
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from users.forms import LoginUserForm, RegisterUserForm
+from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm
 
 
 class LoginUser(LoginView):
@@ -22,3 +27,16 @@ class RegisterUser(CreateView):
     template_name = "users/register.html"
     extra_context = {"title": "Register"}
     success_url = reverse_lazy("users:login")
+
+
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = "users/profile.html"
+    extra_context = {"title": "User's profile"}
+
+    def get_success_url(self) -> str:
+        return reverse_lazy("users:profile")
+
+    def get_object(self, queryset=None) -> Optional[User]:
+        return self.request.user

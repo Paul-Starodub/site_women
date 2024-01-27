0c491594e60c59d53fc8f2191d26b1b9e6cdb544
Paul Starodub <amorallex@gmail.com>
2024-01-27 15:42:31 +0200

    add user registration

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.views import LoginView
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 
-from users.forms import LoginUserForm
+from users.forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -14,3 +16,16 @@ class LoginUser(LoginView):
     ) -> str:  # if we want to use our url instead default,
         # or use LOGIN_REDIRECT_URL in settings.py
         return reverse_lazy("women:home")
+
+
+def register(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data["password"])
+            user.save()
+            return render(request, "users/register_done.html")
+    else:
+        form = RegisterUserForm()
+    return render(request, "users/register.html", {"form": form})
